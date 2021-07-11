c420d18c3896a9ebbd12e36d029e9ba5351a73c0
iamrivera <a3m3rivera@gmail.com>
2021-07-11 11:24:15 -0700

    commit tree 0f6dd523efa310cbb7acd7ac97a913bd6e2d2e9c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchDogImages() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4').then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const urlArray = json['message'];
+        urlArray.forEach(renderDogImages);
+    })
+};
+
+function renderDogImages(url) {
+    const imageGallery = document.getElementById('dog-image-container');
+    let newImg = document.createElement('img')
+    imageGallery.append(newImg);
+    newImg.setAttribute('src', url);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchDogImages();
+    
+});
